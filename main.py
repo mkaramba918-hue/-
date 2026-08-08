4b4884d51e5d088fb723e80ad3ad65399d8fc071
mkaramba918-hue <mkaramba918@gmail.com>
2026-08-08 06:20:24 +0200

    Fix indentation and load shop extension

--- main.py
+++ main.py
@@ -251,6 +251,7 @@ async def handle_specific_role_slash(interaction: discord.Interaction, member: d
 async def on_ready():
     # 1. Загружаем коги
     try:
+     
     await bot.load_extension("cogs.shop")
   await bot.load_extension("cogs.console_logger")
 synced = await bot.tree.sync(guild=guild)
